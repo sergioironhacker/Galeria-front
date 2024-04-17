6036a4762620d34364961f88ab3baae969d38715
Sergio Esteban <Sergioss772022@gmail.com>
2024-04-17 21:10:25 +0200

    gallery

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,28 +1,43 @@
+import  { useState } from 'react';
 import { data } from '../restAPI.json';
 
+
 const Menu = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNext = () => {
+        setCurrentIndex(currentIndex === data[0].dishes.length - 1 ? 0 : currentIndex + 1);
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex(currentIndex === 0 ? data[0].dishes.length - 1 : currentIndex - 1);
+    };
+
     return (
         <section className="menu" id="menu">
             <div className="container">
                 <div className="heading_section">
-                    <h1 className="heading">Galeria</h1>
+                    <h1 className="heading">Galería</h1>
                     <p>Todas mis obras</p>
                 </div>
 
-                <div className="dishes_container">
-                    {data[0].dishes.map((element) => {
-                        return (
-                            <div className="card" key={element.id}>
-                                <img src={element.image} alt={element.title} />
-                                <h1>{element.title}</h1>
-                                <button>{element.category}</button>
-                            </div>
-                        );
-                    })}
+                <div className="carousel">
+                    <div className="image-container">
+                        <img
+                            src={data[0].dishes[currentIndex].image}
+                            alt={data[0].dishes[currentIndex].title}
+                        />
+                    </div>
+                    <button className="nav-btn prev" onClick={handlePrev}>
+                        &#9664; {/* Icono de flecha hacia la izquierda */}
+                    </button>
+                    <button className="nav-btn next" onClick={handleNext}>
+                        &#9654; {/* Icono de flecha hacia la derecha */}
+                    </button>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Menu
+export default Menu;
